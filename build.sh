710156f645e9fe2240af20244686198c0f52c493
unknown <01secn@gmail.com>
2020-10-15 18:22:01 +0100

    Updated

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build --tag simon1000/carbontracker:latest ./trackerproject
-docker push simon1000/carbontracker:latest
-gcloud config set project impact-284220
-gcloud compute instances reset carbontracker --zone=us-central1-a
