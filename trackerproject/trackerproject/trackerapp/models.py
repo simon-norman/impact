2a876fbd4128c91bb5828a453be2aad5c3ab2012
unknown <01secn@gmail.com>
2020-10-22 18:28:38 +0100

    Added journey calc

--- trackerproject/trackerproject/trackerapp/models.py
+++ trackerproject/trackerproject/trackerapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+class Journey(models.Model):
+
 class Location(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
+    carbon_journey_id = models.ForeignKey(Journey, on_delete=models.CASCADE)
