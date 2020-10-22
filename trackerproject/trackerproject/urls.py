30c5b21cd2e29e2b95dde90cd6bec8a584c64925
unknown <01secn@gmail.com>
2020-10-23 00:34:29 +0100

    Health check

--- trackerproject/trackerproject/urls.py
+++ trackerproject/trackerproject/urls.py
@@ -5,5 +5,5 @@ from .trackerapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('locations/', views.LocationList.as_view()),
-    path('/health', views.HealthCheck.as_view()), 
+    path('health', views.HealthCheck.as_view()), 
 ]
